a00d69a9c20625aac625ae8b1a19a742e1508e36
Erol Neuhauß <erol.neuhauss@obi.de>
2025-02-20 17:16:11 +0100

    enable rainbow brackets

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,38 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Treesitter
 
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
+  opts = function(_, opts)
+    -- Ensure Treesitter parsers are installed
+    opts.ensure_installed = opts.ensure_installed or {}
+    vim.list_extend(opts.ensure_installed, {
+      "bash",
+      "dockerfile",
+      "gotmpl",
+      "helm",
+      "ini",
+      "jq",
+      "json",
+      "latex",
+      "make",
+      "markdown",
+      "markdown_inline",
       "lua",
+      "php",
+      "terraform",
+      "todotxt",
       "vim",
-      -- add more arguments for adding more treesitter parsers
-    },
-  },
+      "yaml",
+    })
+
+    -- Enable bracket rainbow highlighting
+    opts.rainbow = {
+      enable = true,
+      extended_mode = true, -- Highlight non-bracket delimiters
+      max_file_lines = 1000, -- Disable for large files
+    }
+  end,
 }
