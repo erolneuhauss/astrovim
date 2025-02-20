aac1e7be7df426962c396becfc69d36867af309a
Erol Neuhauß <erol.neuhauss@obi.de>
2025-02-20 17:17:02 +0100

    configure rainbowDelimiter colors

---
+++ lua/user/highlights.lua
@@ -0,0 +1,9 @@
+vim.cmd [[
+  highlight RainbowDelimiterRed guifg=#E06C75 gui=bold
+  highlight RainbowDelimiterYellow guifg=#E5C07B gui=bold
+  highlight RainbowDelimiterBlue guifg=#61AFEF gui=bold
+  highlight RainbowDelimiterOrange guifg=#D19A66 gui=bold
+  highlight RainbowDelimiterGreen guifg=#98C379 gui=bold
+  highlight RainbowDelimiterViolet guifg=#C678DD gui=bold
+  highlight RainbowDelimiterCyan guifg=#56B6C2 gui=bold
+]]
