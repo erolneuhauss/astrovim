b29e3cee1770d5c8459dc7f81efca8cfb3feb9ac
Erol Neuhauß <erol.neuhauss@gmx.de>
2025-10-01 13:28:32 +0200

    group option for todo-comments.nvim properly

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -21,23 +21,25 @@ return {
     },
     "HiPhish/rainbow-delimiters.nvim", -- Rainbow bracket highlighting for Treesitter
     "diogo464/kubernetes.nvim",
-    "folke/todo-comments.nvim",
-    opts = {
-      keywords = {
-        FIX = {
-          icon = " ", -- icon used for the sign, and in search results
-          color = "error", -- can be a hex color, or a named color (see below)
-          alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "BAD" }, -- a set of other keywords that all map to this FIX keywords
-          -- signs = false, -- configure signs for some keywords individually
+    {
+      "folke/todo-comments.nvim",
+      opts = {
+        keywords = {
+          FIX = {
+            icon = " ", -- icon used for the sign, and in search results
+            color = "error", -- can be a hex color, or a named color (see below)
+            alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "BAD" }, -- a set of other keywords that all map to this FIX keywords
+            -- signs = false, -- configure signs for some keywords individually
+          },
+          TODO = {
+            icon = " ",
+            color = "info",
+            alt = { "GOOD" },
+          },
+          PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE", "DATE" } },
+          SECTION = { icon = " ", alt = { "SUBSECTION" } },
+          REFERENCE = { icon = "", color = "info" },
         },
-        TODO = {
-          icon = " ",
-          color = "info",
-          alt = { "GOOD" },
-        },
-        PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE", "DATE" } },
-        SECTION = { icon = " ", alt = { "SUBSECTION" } },
-        REFERENCE = { icon = "", color = "info" },
       },
     },
   },
